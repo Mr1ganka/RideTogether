320385c32e2d84a0b99c55c6b5d20cfac4cad517
Mr1ganka <103485310+Mr1ganka@users.noreply.github.com>
2026-07-26 02:54:16 +0530

    updated failing pipeline

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:ride_together/app/router/app_router.dart';
-import 'package:ride_together/core/providers/app_lifeCycle_providers.dart';
+import 'package:ride_together/core/providers/app_lifecycle_providers.dart';
 import 'package:ride_together/core/theme/app_theme.dart';
 
 
